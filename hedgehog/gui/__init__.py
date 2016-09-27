31fd26b61fc7c3dc61dd721e6218b66ddd94b275
SillyFreak <sillyfreak.ck@gmail.com>
2016-09-27 14:31:16 +0200

    add try/catch and StreamHandler to reliably show logs & errors even under Windows

--- hedgehog/gui/__init__.py
+++ hedgehog/gui/__init__.py
@@ -1,12 +1,20 @@
+import logging
 import logging.config
+import sys
+import traceback
 
 from .hedgehog_app import HedgehogApp
 
 
 def main():
     logging.config.fileConfig('logging.conf')
+    logging.root.root.addHandler(logging.StreamHandler(sys.stdout))
 
-    HedgehogApp().run()
+    try:
+        HedgehogApp().run()
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise
 
 if __name__ == '__main__':
     main()
